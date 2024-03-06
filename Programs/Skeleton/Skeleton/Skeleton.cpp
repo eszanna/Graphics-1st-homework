eb09f15a1ad97eb8e83a7c5900de759f6b680885
eszanna <131808772+eszanna@users.noreply.github.com>
2024-03-06 08:56:23 +0100

    jporta accepted

--- Programs/Skeleton/Skeleton/Skeleton.cpp
+++ Programs/Skeleton/Skeleton/Skeleton.cpp
@@ -34,7 +34,7 @@
 #include "framework.h"
 
 // vertex shader in GLSL: It is a Raw string (C++11) since it contains new line characters
-const char * const vertexSource = R"(
+const char* const vertexSource = R"(
 	#version 330				// Shader 3.3
 	precision highp float;		// normal floats, makes no difference on desktop computers
 
@@ -47,7 +47,7 @@ const char * const vertexSource = R"(
 )";
 
 // fragment shader in GLSL
-const char * const fragmentSource = R"(
+const char* const fragmentSource = R"(
 	#version 330			// Shader 3.3
 	precision highp float;	// normal floats, makes no difference on desktop computers
 	
@@ -60,21 +60,24 @@ const char * const fragmentSource = R"(
 )";
 
 class PointCollection {
-public: std::vector<GLfloat> vertices;
+public: std::vector<vec3> vertices;
 
-	  void add(float cX, float cY) {// add the clicked point to the vertices
-		  vertices.push_back(cX);
-		  vertices.push_back(cY);
+	  void add(float cX, float cY) {
+		  vertices.push_back(vec3(cX, cY, 1.0f));
 		  printf("Point added at coordinates: %f, %f\n", cX, cY);
 	  }
 
-	  void findClosest(float cX, float cY, PointCollection &selected) {
+	  int getSize() {
+		  return vertices.size();
+	  }
+
+	  void findClosest(float cX, float cY, PointCollection& selected) {
 		  //find the closest point to the clicked location
 		  float threshold = 0.05f; // this can be adjusted
 		  int closestPointIndex = -1;
-		  for (int i = 0; i < vertices.size(); i += 2) {
-			  float dx = vertices[i] - cX;
-			  float dy = vertices[i + 1] - cY;
+		  for (unsigned int i = 0; i < vertices.size(); i++) {
+			  float dx = vertices[i].x - cX;
+			  float dy = vertices[i].y - cY;
 			  if (dx * dx + dy * dy < pow(threshold, 2)) {
 				  closestPointIndex = i;
 				  break;
@@ -82,106 +85,96 @@ public: std::vector<GLfloat> vertices;
 		  }
 		  if (closestPointIndex != -1) {
 			  selected.vertices.push_back(vertices[closestPointIndex]);
-			  selected.vertices.push_back(vertices[closestPointIndex + 1]);
 		  }
 	  }
 
-	  void drawPoints(float size, float R, float G, float B, int &location) {
+	  void drawPoints(float size, float R, float G, float B, int& location) {
 		  glUniform3f(location, R, G, B); // 3 floats
-		  //bind buffer
-		  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0], GL_STATIC_DRAW);
+		  
+		  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vec3), vertices.data(), GL_DYNAMIC_DRAW);
 		  glPointSize(size);
-
-		  glDrawArrays(GL_POINTS, 0, vertices.size() / 2);
+		  glDrawArrays(GL_POINTS, 0, vertices.size());
 	  }
 };
 
 class Line {
 public: float m, b;
-	  bool vertical;
+	  bool vertical; //we need to treat the infinite slope differently
 	  float x_val;
-
 
 	  Line(float m, float b) {
 		  this->m = m;
 		  this->b = b;
-	  };
+	  }
 
-	  Line(float x1, float y1, float x2, float y2) {
-		  if (x1 == x2) {
+	  Line(vec3 point1, vec3 point2) {
+		  if (point1.x == point2.x) {
 			  vertical = true;
-			  x_val = x1;
-
-		}
+			  x_val = point1.x;
+		  }
 		  else {
 			  vertical = false;
-			  m = (y2 - y1) / (x2 - x1);
-			  b = ((-(y2 - y1) / (x2 - x1)) * x1) + y1;
+			  m = (point2.y - point1.y) / (point2.x - point1.x);
+			  b = ((-(point2.y - point1.y) / (point2.x - point1.x)) * point1.x) + point1.y;
 		  }
-	  };
-
+	  }
 };
 
 class LineCollection {
 public: std::vector<Line> lineCollection;
-		Line* selectedline = nullptr;
+	  Line* selectedline = nullptr;
 
-	  void add(Line &l) {
+	  void add(Line l) {
 		  lineCollection.push_back(l);
 	  };
 
-	  void drawLines(float R, float G, float B, int &location) {
-		  std::vector<float> points;
-		  for (int line = 0; line < lineCollection.size(); line++) {
+	  void drawLines(float R, float G, float B, int& location) {
+		  std::vector<vec3> points;
+		  for (unsigned int line = 0; line < lineCollection.size(); line++) {
 			  if (lineCollection[line].vertical == false) {
-				  points.push_back(-1.0f);
-				  points.push_back(lineCollection[line].m * (-1.0f) + lineCollection[line].b);
-				  points.push_back(1.0f);
-				  points.push_back(lineCollection[line].m * (1.0f) + lineCollection[line].b);
+				  points.push_back(vec3(-1.0f, lineCollection[line].m * (-1.0f) + lineCollection[line].b, 0.0f));
+				  points.push_back(vec3(1.0f, lineCollection[line].m * (1.0f) + lineCollection[line].b, 0.0f));
 			  }
 			  else {
-				  points.push_back(lineCollection[line].x_val);
-				  points.push_back(1.0f);
-				  points.push_back(lineCollection[line].x_val);
-				  points.push_back(-1.0f);
+				  points.push_back(vec3(lineCollection[line].x_val, 1.0f, 0.0f));
+				  points.push_back(vec3(lineCollection[line].x_val, -1.0f, 0.0f));
 			  }
-
 		  }
-		  glBufferData(GL_ARRAY_BUFFER, points.size() * sizeof(float), &points[0], GL_STATIC_DRAW);
+		  glBufferData(GL_ARRAY_BUFFER, points.size() * sizeof(vec3), points.data(), GL_STATIC_DRAW);
 		  glUniform3f(location, R, G, B); // 3 floats
 		  glDrawArrays(GL_LINES, 0, points.size());
-	  };
+	  }
 
 	  int getSize() {
 		  return lineCollection.size();
-	  };
+	  }
 
 	  void selectClickedLine(float cX, float cY) {
-		  for (Line &l : lineCollection) {
-			  if (abs(l.m * cX + l.b - cY) < 0.01f) {
-				  printf("Line selected at point %f %f\n", cX, cY); //for debug
+		  for (Line& l : lineCollection) {
+			  float sol = l.m * cX + l.b - cY;
+			  if (sqrt(pow(sol, 2)) < 0.05f) {
 				  selectedline = &l;
 				  break;
 			  }
-		 }
-	  };
+		  }
+	  }
 
 	  void dragSelectedLine(float cX, float cY) {
-		  if (selectedline) { 
+		  if (selectedline) {
 			  selectedline->b = cY - selectedline->m * cX; //modify the +b part of the equation, slope stays the same
+
 		  }
-	  };
+	  }
 
 	  void selectTwoLines(float cX, float cY, LineCollection& selected) {
-			  for (Line& l : lineCollection) {
-				  if (abs(l.m * cX + l.b - cY) < 0.05f) {
-					  printf("Line selected at point %f %f\n", cX, cY); //for debug
-					  selected.add(l);
-					  break;
-				  }
+		  for (Line& l : lineCollection) {
+			  float sol = l.m * cX + l.b - cY;
+			  if (sqrt(pow(sol, 2)) < 0.01f) {
+				  selected.add(l);
+				  break;
 			  }
-		  
-	  };
+		  }
+	  }
 
 	  void addPointOfIntersection(Line l1, Line l2, PointCollection& pointcollection) {
 		  if (l1.m == l2.m) { //they are parallel, no intersection
@@ -212,7 +205,7 @@ void onInitialization() {
 	glBindBuffer(GL_ARRAY_BUFFER, vboPoints);
 	glEnableVertexAttribArray(0);  // AttribArray 0
 	glVertexAttribPointer(0,       // vbo -> AttribArray 0
-		2, GL_FLOAT, GL_FALSE, // two floats/attrib, not fixed-point
+		3, GL_FLOAT, GL_FALSE, // three floats/attrib, not fixed-point
 		0, NULL); 		     // stride, offset: tightly packed
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
@@ -224,7 +217,7 @@ PointCollection points; //here I store the coordinates x and y of every drawn po
 PointCollection selected; //these will be the points of the lines
 
 LineCollection collection;
-LineCollection selectedLines;
+LineCollection selectedLines; //always two, to find intersection
 
 // Window has become invalid: Redraw
 void onDisplay() {
@@ -246,34 +239,19 @@ void onDisplay() {
 
 	glBindVertexArray(vao);
 
-		// Draw points
-		glBindBuffer(GL_ARRAY_BUFFER, vboPoints);  // Draw call
-		points.drawPoints(10.0f, 1.0f, 0.0f, 0.0f, colorloc);
+	// Draw points
+	glBindBuffer(GL_ARRAY_BUFFER, vboPoints);  // Draw call
+	points.drawPoints(10.0f, 1.0f, 0.0f, 0.0f, colorloc);
 
-		if (lKeyPressed) { //we dont want to add lines when we drag an existing one
-			// Draw lines 
-			if (selected.vertices.size() >= 4 ) {
-			
-					for (int i = 0; i < selected.vertices.size(); i += 4) { //we add every 2 pair of points, but only when we indeed have 2 pairs selected
-						if (i + 4 == selected.vertices.size()) {
-							Line line(selected.vertices[i], selected.vertices[i + 1], selected.vertices[i + 2], selected.vertices[i + 3]);
-							collection.add(line);
-							printf("Equation of the line: y = %f x + %f\n", line.m, line.b);
-						}
-					}
-			}
-		}
-		
-			if (selectedLines.getSize() == 2) {
-				selectedLines.addPointOfIntersection(selectedLines.lineCollection[0], selectedLines.lineCollection[1], points);
-				selectedLines.lineCollection.clear();
-				printf("Point of intersection is added if it existed.");
-			}
-		
-		glLineWidth(3.0f);		
-		collection.drawLines(0.0f, 1.0f, 1.0f, colorloc);
-			
-		glutSwapBuffers(); // exchange buffers for double buffering
+
+	//if we have two lines selected, find the intersection
+	
+
+	glLineWidth(3.0f);
+
+	collection.drawLines(0.0f, 1.0f, 1.0f, colorloc);
+
+	glutSwapBuffers(); // exchange buffers for double buffering
 }
 
 // Key of ASCII code pressed
@@ -322,18 +300,38 @@ void onMouse(int button, int state, int pX, int pY) {
 	if (state == GLUT_UP) { //so when we let go of the selected line, at the next click we can select a new one and forget the previously selected
 		collection.selectedline = nullptr;
 	}
-	else if (state == GLUT_DOWN) {
+
+	if (state == GLUT_DOWN) {
 		if (pKeyPressed) {
 			points.add(cX, cY);
+			glutPostRedisplay();
 		}
 		else if (lKeyPressed) {
 			points.findClosest(cX, cY, selected);
+			if (selected.vertices.size() == 2) {
+				Line line(selected.vertices[0], selected.vertices[1]);
+				collection.add(line);
+
+				
+				glutPostRedisplay();
+
+				printf("Implicit equation of the line: -%f x - %f + y = 0\n", line.m, line.b);
+				printf("Parametric equation of the line: r<t> = <%f,%f> + <1,%f> t\n", selected.vertices[0].x, selected.vertices[0].y, line.m);
+				selected.vertices.clear();
+			}
+			glutPostRedisplay();
 		}
 		else if (mKeyPressed) {
 			collection.selectClickedLine(cX, cY);
+			glutPostRedisplay();
 		}
 		else if (iKeyPressed) {
-			collection.selectTwoLines(cX, cY, selectedLines); //twice because we need two lines
+			collection.selectTwoLines(cX, cY, selectedLines);
+			if (selectedLines.getSize() == 2) {
+				selectedLines.addPointOfIntersection(selectedLines.lineCollection[0], selectedLines.lineCollection[1], points);
+				selectedLines.lineCollection.clear();
+				printf("Point of intersection is added (if it existed).\n");
+			}
 			glutPostRedisplay();
 		}
 	}
@@ -344,8 +342,6 @@ void onMouseMotion(int pX, int pY) {	// pX, pY are the pixel coordinates of the 
 	// Convert to normalized device space
 	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
 	float cY = 1.0f - 2.0f * pY / windowHeight;
-	//printf("Mouse moved to (%3.2f, %3.2f)\n", cX, cY);
-
 	collection.dragSelectedLine(cX, cY);
 	glutPostRedisplay();
 }
